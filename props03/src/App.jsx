8a97519a709a895ff3e19cc8b52138cb85891621
Adarsh-c-byte <vsgs55409@gmail.com>
2026-06-02 20:10:24 +0530

    props03 done

---
+++ props03/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './component/Card'
+const App = () => {
+  return (
+    <div className="parent">
+      <Card user="Pinku pappu" img="https://plus.unsplash.com/premium_photo-1661627681947-4431c8c97659?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user="Bittu baklol" img="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <Card user="Raju rabdi" img="https://images.unsplash.com/photo-1548372290-8d01b6c8e78c?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      
+    </div>
+  )
+}
+
+export default App
---
+++ props03/src/component/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    
+      
+        <div className="Card">
+          <img src={props.img} alt="" />
+          <h1>{props.user}</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae, fuga?</p>
+          <button>view profile</button>
+        </div>
+     
+   
+  )
+}
+
+export default Card
